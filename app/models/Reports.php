6916c7357746a85034ee259017a03c496a756f50
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-07-18 21:18:51 +0000

    implementer getTopReminderUser() to identify which user has created the most reminders

--- app/models/Reports.php
+++ app/models/Reports.php
@@ -16,4 +16,17 @@ class Report {
         ";
         return $this->db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function getTopReminderUser() {
+        $sql = "
+          SELECT u.username, COUNT(r.id) AS cnt
+          FROM reminders r
+          JOIN users u ON r.user_id = u.id
+          GROUP BY u.username
+          ORDER BY cnt DESC
+          LIMIT 1
+        ";
+        return $this->db->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+    
 }
