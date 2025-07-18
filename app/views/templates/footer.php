22eba73833bd4d87f1431f67a60b44077c297fa7
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-07-18 21:59:25 +0000

    added a dynamic footer displaying current year home page and my github link and bootstrap js bundle link.

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,13 +1,13 @@
-<footer class="footer">    
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; <?php echo date('Y'); ?> </p>
-        </div>
-    </div>
+<footer class="bg-light text-center py-4 mt-auto">
+  <div class="container">
+    <p class="mb-1">&copy; <?= date('Y') ?> Syed Nuaym Nadeem</p>
+    <nav>
+      <a href="/">Home</a> |
+      <a href="https://github.com/syednuaymnadeem/cosc-4806-5" target="_blank" rel="noopener">GitHub</a>
+    </nav>
+  </div>
 </footer>
 
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
