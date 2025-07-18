4ba9418890b0712e6b5a45039a6c4741b2e31736
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-07-18 20:38:05 +0000

    added admin-only access using if statement

--- app/controllers/reports.php
+++ app/controllers/reports.php
@@ -3,6 +3,12 @@ class Reports extends Controller {
   public function __construct() {
       parent::__construct();
 
+    if (empty($_SESSION['username']) || 
+        SESSION['username'] !== 'admin') {
+        header('Location: /home');
+        exit;
+    }
+
       
 
   }
